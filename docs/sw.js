4a36a629f8f444b7cc510dd229cb5580649b3fb0
LuizFernando1176 <nando1176@hotmail.com>
2026-02-18 15:39:12 -0300

    Versão definitiva GitHub Pages PWA funcionando

--- docs/sw.js
+++ docs/sw.js
@@ -36,3 +36,46 @@ self.addEventListener("activate", () => {
 self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
+const CACHE_NAME = "portal-quaresma-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./quaresma.json",
+  "./manifest.webmanifest",
+  "./icons/icon-gold-192.png",
+  "./icons/icon-gold-512.png",
+  "./icons/badge-96.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
